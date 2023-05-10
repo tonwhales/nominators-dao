2d3722c1b92cc1edc504bcef32dbdc74e34e18e2
Dan Volkov <hi@dvlkv.com>
2023-05-10 11:43:57 +0400

    wip: update apy proposal for Tonkeeper

---
+++ partners/tonkeeper/proposals/1-update-apy.ts
@@ -0,0 +1,56 @@
+import { Address, toNano } from 'ton-core';
+import { pools, vanities } from '../../addresses';
+import { randomQueryId } from '../../../utils/queryId';
+import { createChangeAddressCommand, createUpdateExtrasCommand } from '../../../utils/nominators';
+import { createProposalPayload, proposedMessage } from '../../../utils/proposal';
+
+const keeperParams1 = {
+    enabled: true,
+    enabledUpgrades: true,
+    depositFee: toNano(0.1),
+    withdrawFee: toNano(0.1),
+    poolFee: 25n * 100n,
+    receiptPrice: toNano(0.1),
+    minStake: toNano('49.8'),
+};
+const keeperParams2 = {
+    enabled: true,
+    enabledUpgrades: false,
+    depositFee: toNano(0.1),
+    withdrawFee: toNano(0.1),
+    poolFee: 25n * 100n,
+    receiptPrice: toNano(0.1),
+    minStake: toNano('49.8'),
+};
+
+function main() {
+    let queryId = randomQueryId();
+
+    let config = [{
+        dao: vanities.v1.daoKeeper1v2.address,
+        pool: pools.keeper1,
+        extras: keeperParams1,
+    }, {
+        dao: vanities.v1.daoKeeper2v2.address,
+        pool: pools.keeper2,
+        extras: keeperParams2,
+    }];
+
+
+    console.log('Update fees to 25%');
+    console.log();
+
+    for (let cfg of config) {
+        let updateExtras = createUpdateExtrasCommand(queryId, cfg.extras);
+        let proposal = createProposalPayload(queryId, [
+            proposedMessage(updateExtras, BigInt(500_000_000), cfg.pool),
+        ]);
+
+        console.log('DAO: ', cfg.dao.toString());
+        console.log('Proposal: ', proposal.toBoc().toString('base64'));
+        console.log();
+        console.log();
+    }
+}
+
+main();
--- utils/nominators.ts
+++ utils/nominators.ts
@@ -1,5 +1,28 @@
 import { Address, beginCell } from 'ton-core';
 
+type Extras = {
+    enabled: boolean,
+    enabledUpgrades: boolean,
+    minStake: bigint,
+    depositFee: bigint,
+    withdrawFee: bigint,
+    poolFee: bigint,
+    receiptPrice: bigint
+};
+
+function buildExtrasCell(extras: Extras) {
+    const ex = beginCell()
+        .storeBit(extras.enabled)
+        .storeBit(extras.enabledUpgrades)
+        .storeCoins(extras.minStake)
+        .storeCoins(extras.depositFee)
+        .storeCoins(extras.withdrawFee)
+        .storeCoins(extras.poolFee)
+        .storeCoins(extras.receiptPrice)
+        .endCell();
+    return ex;
+}
+
 export function createChangeAddressCommand(queryId: bigint, index: number, address: Address) {
     return beginCell()
         .storeUint(2431318753, 32)
@@ -8,4 +31,17 @@ export function createChangeAddressCommand(queryId: bigint, index: number, addre
         .storeUint(index, 8)
         .storeAddress(address)
         .endCell();
+}
+
+export function createUpdateExtrasCommand(queryId: bigint, extras: Extras) {
+    let ex = buildExtrasCell(extras);
+
+    const command = beginCell()
+        .storeUint(37541164, 32)
+        .storeUint(queryId, 64) // Query ID
+        .storeCoins(100000)
+        .storeRef(ex)
+        .endCell(); // Gas
+
+    return command;
 }
