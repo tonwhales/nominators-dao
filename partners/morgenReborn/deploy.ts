e51ff8693345865e43edd67f9732310a15df74f7
Garrethta <degtyarev2000@inbox.ru>
2024-07-03 13:30:26 +0400

    fix: project struct

--- partners/morgenReborn/deploy.ts
+++ partners/morgenReborn/deploy.ts
@@ -3,16 +3,16 @@ import { printDeployParams } from '../../utils/print';
 
 async function main() {
     await printDeployParams(
-        'Morgen #1',
-        vanities.v1.daoMorgen1,
-        pools.morgen1,
+        'Mafia #1',
+        vanities.v1.daoMorgen2_1,
+        pools.morgen2_1,
         morgenMembers,
     );
 
     await printDeployParams(
-        'Morgen #2',
-        vanities.v1.daoMorgen2,
-        pools.morgen2,
+        'Mafia #2',
+        vanities.v1.daoMorgen2_2,
+        pools.morgen2_2,
         morgenMembers,
     );
 }
